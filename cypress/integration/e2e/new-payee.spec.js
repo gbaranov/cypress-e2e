d0d6e45ffa9f8348052283f1be8fd1421adaa138
Georgy Baranov <floridavbv@gmail.com>
2021-12-05 11:10:42 -0700

    e2e login tests

---
+++ cypress/integration/e2e/new-payee.spec.js
@@ -0,0 +1,33 @@
+describe('New Payee Test', () => {
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.fixture('user').then(user => {
+            const username = user.id
+            const password = user.pwd
+
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.get('#user_remember_me').click()
+            cy.contains('Sign in').click()
+        })
+    })
+
+    it('Add new payee to the list', () => {
+        cy.get('#pay_bills_tab').click()
+        cy.contains('Add New Payee').click()
+        cy.get('#np_new_payee_name').type('Name')
+        cy.get('#np_new_payee_address').type('Address')
+        cy.get('#np_new_payee_account').type('123456789')
+        cy.get('#np_new_payee_details').type('detail')
+        cy.get('#np_new_payee_payee').click()
+
+    })
+
+    it('Success message displayed', () => {
+        cy.get('#alert_content')
+            .should('be.visible')
+            .and('contain', 'The new payee Name was')
+
+    })
+})
